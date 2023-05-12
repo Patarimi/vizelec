b019d2ebdb3daba5e5ab58c9f318d05103f7bde0
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-05-12 11:56:41 +0200

    refac parse.py for a more universal usage.

--- vizelec/parse.py
+++ vizelec/parse.py
@@ -1,10 +1,20 @@
 from lark import Lark, Transformer
+from os.path import join, splitext
 
 
-def parse_verilog(verilog_file: str):
-    with open("vizelec/ebnf/verilog", "r") as f:
+def parse(file: str):
+    base_template = "vizelec/ebnf"
+    _, ext = splitext(file)
+    template = ""
+    if ext == ".v":
+        template = "verilog"
+    if ext in (".cir", ".sp"):
+        template = "spice"
+    if template == "":
+        raise ValueError(f"File extension {ext} unknown.")
+    with open(join(base_template, template), "r") as f:
         verilog_parser = Lark(f)
-    with open(verilog_file) as f:
+    with open(file) as f:
         t = verilog_parser.parse(f.read())
     print(VerilogTransform().transform(t))
     return t
