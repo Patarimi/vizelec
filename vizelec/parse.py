3c5f9caffc5bab3129208b23d055a22fdcb9142b
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-05-31 19:42:09 +0200

    Rewrite parse with match-case

--- vizelec/parse.py
+++ vizelec/parse.py
@@ -5,13 +5,13 @@ from os.path import join, splitext
 def parse(file: str):
     base_template = "vizelec/ebnf"
     _, ext = splitext(file)
-    template = ""
-    if ext == ".v":
-        template = "verilog"
-    if ext in (".cir", ".sp"):
-        template = "spice"
-    if template == "":
-        raise ValueError(f"File extension {ext} unknown.")
+    match ext:
+        case ".v":
+            template = "verilog"
+        case ".cir" | ".sp":
+            template = "spice"
+        case _:
+            raise ValueError(f"File extension {ext} unknown.")
     with open(join(base_template, template), "r") as f:
         verilog_parser = Lark(f)
     with open(file) as f:
