9745554c6ac319771da6fff780e7f6d57b9fe92d
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-06-01 19:46:18 +0200

    Interactive mode

--- vizelec/stick_draw.py
+++ vizelec/stick_draw.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import typer
 
 
-well = {"space": 10, "n": 2}
+app = typer.Typer()
+
 margin = 10
-
-mos_d = {"y": 15, "x": 5, "finger": 6, "poly": {"ext": 10}, "active": {"ext": 10}}
 
 rail = {"y": 5, "space": 3}
 
@@ -22,7 +22,7 @@ def draw_mos(axis, mos: dict, offset: tuple[int, int] = (0, 0), doping: str = "n
         )
     )
     for i in range(mos["finger"]):
-        ax.add_patch(
+        axis.add_patch(
             Rectangle(
                 (offset[0] + (i + 1) * mos["active"]["ext"] + i * mos["x"], offset[1]),
                 mos["x"],
@@ -32,7 +32,7 @@ def draw_mos(axis, mos: dict, offset: tuple[int, int] = (0, 0), doping: str = "n
             )
         )
 
-    ax.add_patch(
+    axis.add_patch(
         Rectangle(
             (
                 offset[0],
@@ -48,22 +48,44 @@ def draw_mos(axis, mos: dict, offset: tuple[int, int] = (0, 0), doping: str = "n
     )
 
 
-fig, ax = plt.subplots()
-draw_mos(ax, mos_d)
-draw_mos(
-    ax, mos_d, (0, well["space"] + mos_d["y"] + 2 * mos_d["poly"]["ext"]), doping="p"
-)
-plt.xlim(
-    -margin,
-    mos_d["finger"] * mos_d["x"]
-    + (mos_d["finger"] + 1) * mos_d["active"]["ext"]
-    + margin,
-)
-plt.ylim(
-    -margin,
-    well["n"] * (mos_d["y"] + 2 * mos_d["poly"]["ext"])
-    + (well["n"] - 1) * well["space"]
-    + margin,
-)
-plt.axis("off")
-plt.show()
+@app.command()
+def draw_canvas(n_well_n, n_well_p, finger):
+    fig, ax = plt.subplots()
+    mos_d = {
+        "y": 15,
+        "x": 5,
+        "finger": finger,
+        "poly": {"ext": 10},
+        "active": {"ext": 10},
+    }
+    well = {"space": 10, "n": n_well_n + n_well_p}  # TODO: manage well number
+    draw_mos(ax, mos_d)
+    draw_mos(
+        ax,
+        mos_d,
+        (0, well["space"] + mos_d["y"] + 2 * mos_d["poly"]["ext"]),
+        doping="p",
+    )
+    plt.xlim(
+        -margin,
+        mos_d["finger"] * mos_d["x"]
+        + (mos_d["finger"] + 1) * mos_d["active"]["ext"]
+        + margin,
+    )
+    plt.ylim(
+        -margin,
+        well["n"] * (mos_d["y"] + 2 * mos_d["poly"]["ext"])
+        + (well["n"] - 1) * well["space"]
+        + margin,
+    )
+    plt.axis("off")
+    plt.show(block=False)
+    plt.waitforbuttonpress()
+
+
+@app.command("main")
+def flow():
+    finger = typer.prompt("maximum finger number", type=int)
+    n_well_n = typer.prompt("number of n-well rows", type=int)
+    n_well_p = typer.prompt("number of p-well rows", type=int)
+    draw_canvas(n_well_n, n_well_p, finger)
