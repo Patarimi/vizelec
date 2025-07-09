67022ba5f99824c07dcbd2839a0287771530ae28
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-07-09 17:16:10 +0200

    add support for more spice command

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -8,5 +8,6 @@ def test_verilog():
 
 def test_spice():
     tree = ps.parse("./tests/inv.cir")
-    assert tree.children[0].data == "v_source"
+    assert tree.children[0].data == "title"
+    assert tree.children[1].data == "v_source"
     assert tree.children[1].children[0] == "in"
